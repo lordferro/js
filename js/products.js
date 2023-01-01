89c7d5e665b600bbbc5911afd076f3a1375cffbb
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-01-01 20:07:07 +0100

    1

---
+++ js/products.js
@@ -0,0 +1,42 @@
+export default
+[{
+  name: "Сервоприводы1",
+  description:
+    "dfg drsdlfkjhlkldszdczdcjlfjlkjfkjvdfhg",
+  price: 2000,
+  available: true,
+  onSale: true,
+},
+{
+  name: "Сервоприводы2",
+  description:
+    "dfg drsdlfkjhlkldszdcdcjlfjlkjfkjvdfhg",
+  price: 2000,
+  available: true,
+  onSale: true,
+},
+{
+  name: "Сервоприводы3",
+  description:
+    "dfg drsdlfkjhlkldsjlfzdczdcjlkjfkjvdfhg",
+  price: 2000,
+  available: false,
+  onSale: false,
+},
+{
+  name: "Сервоприводы4",
+  description:
+    "dfg drsdlfkjhlkldsjlfjlzczdsckjfkjvdfhg",
+  price: 2000,
+  available: true,
+  onSale: false,
+},
+{
+  name: "Сервоприводы5",
+  description:
+    "dfg drsdlfkjhlkldsjlfjlkjfkjxvxzvvdfhg",
+  price: 2000,
+  available: false,
+  onSale: false,
+},
+]
